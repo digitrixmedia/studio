41baaf50b3d8e5ef4b8c35ae3644229ea6cba23b
Digitrix Media <digitrixmedia.agency@gmail.com>
2025-11-16 13:46:24 +0000

    File changes

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,13 +1,15 @@
-
 'use client';
 
 import { AppContextProvider } from '@/contexts/AppContext';
 import { SettingsProvider } from '@/contexts/SettingsContext';
+import { FirebaseClientProvider } from '@/firebase';
 
 export function AppProvider({ children }: { children: React.ReactNode }) {
   return (
-    <SettingsProvider>
-      <AppContextProvider>{children}</AppContextProvider>
-    </SettingsProvider>
+    <FirebaseClientProvider>
+      <SettingsProvider>
+        <AppContextProvider>{children}</AppContextProvider>
+      </SettingsProvider>
+    </FirebaseClientProvider>
   );
 }
