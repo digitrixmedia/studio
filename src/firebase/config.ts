f32d50da74c5b21fd0a2322fd2f3830445212387
Digitrix Media <digitrixmedia.agency@gmail.com>
2025-11-16 13:45:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-7098858483-56ed2",
+  "appId": "1:4077205424:web:6d15ecf5647055e6c40875",
+  "apiKey": "AIzaSyDeO8vu4e5V_E-T5CDGu0NjHLCVsWt7JLU",
+  "authDomain": "studio-7098858483-56ed2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "4077205424"
+};
